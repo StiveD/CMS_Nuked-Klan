6f12f70a470343da72f6ae14917680ed9c016688
Stive Determe <stivedeterme@msn.com>
2013-12-01 13:59:00 +0100

    mod User Update index.php - Update nkDefault.js - Add function index for index.php - Add function initDivSystem for nkDefault.js - add function userModalFull  for nkDefault.js - add function closeModalFull  for nkDefault.js - add function modsUsers  for nkDefault.js

--- assets/scripts/nkDefault.js
+++ assets/scripts/nkDefault.js
@@ -1,6 +1,109 @@
 $(document).ready(function(){
+    // -> all Initialize function
+    initDivSystem();
+    // -> close popup private message
     $('#nkNewPrivateMsgClose').click(function(){
         document.cookie = "popup=false";
         $(this).parent().slideUp(200);
     });
+    // -> Redirect link
+    $('a').click(function() {
+        var hrefLink = $(this).attr('href');
+
+        if (hrefLink == 'index.php?file=User') {
+            var dataHref = 'index.php?file=User&nuked_nude=index&op=index';
+            $(this).attr('data-title', 'Déconnexion').attr('href', dataHref);
+            userModalFull(this);
+            $(this).attr('href', hrefLink);
+            return false;
+        }
+    });
 });
+
+// -> Fuction jquery
+    // -> Initialize nkDialog
+    function initDivSystem(){
+        $('<div id="nkDialog"></div>').prependTo('body');
+    }
+    // -> user modal fullscreen
+    function userModalFull(getUrl) {
+        // -> add style for user
+        $("head").append( $(document.createElement("link")).attr({rel:"stylesheet", type:"text/css", href:"modules/User/User.css"}));
+        // -> ajax
+        $.ajax({
+            type: 'GET',
+            url: getUrl,
+            // -> success
+            success:function(data) {
+                $('#nkDialog').html(data);
+                $('#nkDialog').css({
+                    display: 'none',
+                    padding: '0',
+                    position: 'fixed',
+                    left: 0,
+                    right: 0,
+                    zIndex: 600000,
+                })
+                .fadeIn(500);
+                $('body').css({overflow: 'hidden'});
+                modsUsers();
+                tooltips();
+            }, // -> end success
+            // -> error
+            error:function(){
+                $('#nkDialog').html("ERROR !");
+            }// -> error
+        }); // -> end ajax
+
+        closeModalFull();
+    }
+    // -> user modal close
+    function closeModalFull() {
+        $(document).keyup(function(e) {
+            if (e.keyCode == 27) {
+                $('#nkDialog').fadeOut(1000);
+                $('body').css({overflow: 'auto'});
+            }
+        });
+        $('#jqueryClose').click(function() {
+            $('#nkDialog').fadeOut(1000);
+            $('body').css({overflow: 'auto'});
+            return false;
+        });
+    }
+    function modsUsers() {
+        closeModalFull();
+        // -> Variables
+        animateHoverListFriends = false;
+        // -> tabs
+        $('#tab-container').easytabs({
+            animationSpeed: 300,
+            collapsible: false,
+            tabActiveClass: "clicked"
+        });
+        $('.jqueryLinksSwtich').each(function() {
+            $(this).click(function(event) {
+                event.preventDefault();
+                // -> Variables
+                var value  = $(this).attr('href').replace('#','');
+                var getUrl = 'index.php?file=User&nuked_nude=index&op=' + value;
+                var title  = $(this).data('title');
+                var icon   = $(this).data('icon');
+                $('#jquerySections').fadeOut(450, function() {
+                    // -> ajax
+                    $.ajax({
+                        type: 'GET',
+                        url: getUrl,
+                        // -> success
+                        success:function(data) {
+                            $('#jquerySections').empty().append(data).fadeIn(450);
+                            modsUsers();
+                            tooltips();
+                        } // -> end success
+                    }); // -> end ajax
+                });
+                $('#jqueryTitle').empty().append(title);
+                $('#jqueryIcon').removeAttr('class').attr('class', icon);
+            });
+        });
+    }
--- modules/User/index.php
+++ modules/User/index.php
@@ -12,4 +12,102 @@ if (!defined("INDEX_CHECK")) exit('You can\'t run this file alone.');
 
 nkTranslate('modules/User/lang/'.$GLOBALS['language'].'.lang.php');
 
+    function index () {
+        if (!nkHasVisitor()):
+?>
+        <section id="modsUser">
+            <aside>
+                <nav>
+                    <ul>
+                        <li class="pseudo">
+                            <img src="http://www.palacewar.eu/Stive/avatar_stive2.png" alt="Stive">
+                            <span>Stive</span>
+                        </li>
+                        <li>
+                            <a class="icon-home jqueryLinksSwtich" data-icon="icon-home" data-title="Accueil" href="#Home">Accueil</a>
+                        </li>
+                        <li>
+                            <a class="icon-profile jqueryLinksSwtich" data-icon="icon-profile" data-title="Mon Compte" href="#MyAccount">Mon Compte</a>
+                        </li>
+                        <li>
+                            <a class="icon-envelope jqueryLinksSwtich" data-icon="icon-envelope" data-title="Messagerie Priv&eacute;" href="#PrivateMsg">Messagerie Priv&eacute;
+                                <span>14</span>
+                            </a>
+                            <ul id="userbox">
+                                <li><a class="icon-arrow-right" href="#">Boîte de r&eacute;ception</a></li>
+                                <li><a class="icon-arrow-right" href="#">Envoy&eacute;(s)</a></li>
+                                <li><a class="icon-arrow-right" href="#">Corbeille</a></li>
+                            </ul>
+                        </li>
+                        <li id="jqueryListFriends"s>
+                            <a class="icon-users" href="#">Liste d'amis (3)<span>0</span></a>
+                            <ul id="listFriends">
+                                <li><a class="friend" href="#">Liste d'amis (3)</a></li>
+                                <li>
+                                    <a href="#" class="onlineGreen">
+                                        <img src="http://www.nuked-klan.org/upload/User/1370708754.jpg" alt="Homax">
+                                        <span>Homax</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="onlineRed">
+                                        <img src="http://images.samoth.fr/photos/sam.png" alt="Samoth">
+                                        <span>Samoth</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="onlineRed">
+                                        <img src="http://www.nuked-klan.org/upload/Forum/d868bc8969.png" alt="Zdav">
+                                        <span>Zdav</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+
+            </aside>
+
+            <article>
+
+                <header>
+                    <h1>Compte</h1>
+                    <a class="tipE" href="#" title="Pr&eacute;f&eacute;rence"><span class="icon-cog"></span></a>
+                    <a class="tipE" href="#" title="D&eacute;connexion" id="jqueryClose"><span class="icon-power-outline"></span></a>
+                </header>
+                <nav id="breadcrumb">
+                    <ul>
+                        <li>
+                            <span id="jqueryIcon" class="icon-home"></span>
+                        </li>
+                        <li id="jqueryTitle">Accueil</li>
+                    </ul>
+                </nav>
+                <section id="jquerySections">
+                    <?php Home(); ?>
+                </section>
+            </article>
+
+        </section>
+<?php
+        else:
+            redirect("index.php?file=User&op=login_screen", 0);
+        endif;
+    }
+
+    // -> backward compatibility for Nk < 1.8
+    if (!array_key_exists('nuked_nude', $_REQUEST)) { opentable(); }
+
+    switch ($_REQUEST['op']) {
+        case"index":
+             index();
+             break;
+
+        default:
+            index();
+            break;
+    }
+
+    if (!array_key_exists('nuked_nude', $_REQUEST)) { closetable(); }
+?>
 ?>
